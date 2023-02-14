fc56cebedc1bc61e779f1d8acda364ebc85a8bfc
manuel101284 <manuel101284@gmail.com>
2023-02-14 15:59:08 -0500

    R03: Correción directorios

--- back/index.js
+++ back/index.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const PORT = 3030;
 const app = express();
 
-const todoRoutes = require("./routes/todoRoutes");
+const todoRoutes = require("./back/routes/todoRoutes");
 const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true };
 //const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://manuelricardo:castellanos@cluster-todolist.vhnrpzx.mongodb.net/test", connectionOptions)
+mongoose.connect("mongodb + srv://manuelricardo:castellanos@cluster0.pcwd49f.mongodb.net/?retryWrites=true&w=majority", connectionOptions)
     //mongoose.connect("mongodb://localhost:27017/To-Do-List-Project", connectionOptions)
     .then(() => console.log("The Conection is OK"))
     .catch((err) => console.error(err));
